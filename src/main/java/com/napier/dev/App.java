72720f9899dba028bc8edece79e3a06885a1b531
PaingThetKyaw <40632014@live.napier.ac.uk>
2024-02-01 10:19:16 +0630

    Second SQL working and user input is accepted. getCitiesByCont()

--- src/main/java/com/napier/dev/App.java
+++ src/main/java/com/napier/dev/App.java
@@ -1,6 +1,9 @@
 package com.napier.dev;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.sql.*;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class App {
     private Connection con = null;
@@ -70,6 +73,36 @@ public class App {
             return null;
         }
     }
+    public  ArrayList<World> getCitiesByCont(String continentName){
+        try {
+            Statement stmt = con.createStatement();
+            String strSelect =
+                    "SELECT city.Name AS Name, country.Name AS Country, city.District, " +
+                            "city.Population " +
+                            "FROM world.city " +
+                            "JOIN world.country ON city.CountryCode = country.Code " +
+                            "WHERE country.Continent = '" + continentName + "' " +
+                            "ORDER BY city.Population DESC";
+
+            ResultSet rset = stmt.executeQuery(strSelect);
+            ArrayList<World> citybycont = new ArrayList<World>();
+            while (rset.next()) {
+                World wrld = new World();
+                wrld.CityName = rset.getString("Name");
+                wrld.CountryName = rset.getString("Country");
+                wrld.District = rset.getString("District");
+                wrld.CityPopulation = rset.getInt("Population");
+                citybycont.add(wrld);
+
+            }
+            return citybycont;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get cities in world details");
+            return null;
+        }
+
+    }
 
     // Print out the data
     public void printOutput(ArrayList<World> country) {
@@ -85,6 +118,19 @@ public class App {
             System.out.println(wrld_str);
         }
     }
+    public void DisplayCities(ArrayList<World> city) {
+        // Print header
+        System.out.println(String.format("%-37s %-49s %-23s %-25s",
+                "Name", "Country", "District", "Population"));
+
+        // Loop over all cities in the list
+        for (World wrld : city) {
+            String wrld_str =
+                    String.format("%-37s %-49s %-23s %-25s",
+                            wrld.CityName, wrld.CountryName, wrld.District, wrld.CityPopulation);
+            System.out.println(wrld_str);
+        }
+    }
 
     public static void main(String[] args) {
         // Create new Application
@@ -93,9 +139,36 @@ public class App {
         // Connect to database
         a.connect();
 
-        // Extract world details from the database
-        ArrayList<World> country = a.getCountryWorld();
-        a.printOutput(country);
+        // Prompt user for choice
+        // Prompt user for choice
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            System.out.println("Choose a function:");
+            System.out.println("1. Countries\n"
+            + "2. Cities");
+
+            // Read user input
+            int choice = Integer.parseInt(reader.readLine());
+
+            // Execute chosen function
+            switch (choice) {
+                case 1:
+                    // Extract world details from the database
+                    ArrayList<World> country = a.getCountryWorld();
+                    a.printOutput(country);
+                    break;
+                case 2:
+                    System.out.println("Choose a Continent");
+                    String contChoice = reader.readLine();
+                    ArrayList<World> CT = a.getCitiesByCont(contChoice);
+                    a.DisplayCities(CT);
+                    break;
+                default:
+                    System.out.println("Invalid choice");
+            }
+        } catch (Exception e) {
+            System.out.println("Error reading input: " + e.getMessage());
+        }
+
 
         // Disconnect from database
         a.disconnect();
